36d52d3869fc1cdc15882c29be67acc9fddf432a
uuzor <victor.u.ezealor@gmail.com>
2024-10-07 18:32:34 +0100

    added learn

--- src/dist/script.js
+++ src/dist/script.js
@@ -1,4 +1,4 @@
-document.getElementById("menu").addEventListener("click", function(e){
+document.getElementById("menu").addEventListener("focus", function(e){
     e.preventDefault();
    
     
@@ -9,7 +9,7 @@ document.getElementById("menu").addEventListener("click", function(e){
     
 })
 
-document.getElementById("menu").addEventListener("body", (e)=> {
+document.getElementById("menu").addEventListener("click", (e)=> {
 
     e.preventDefault();
     var menubar = document.getElementById("menubar");
