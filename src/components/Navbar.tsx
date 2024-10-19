03accf352ca8f2d826827305807eb54f56b860d8
uuzor <victor.u.ezealor@gmail.com>
2024-10-19 11:45:36 +0100

    commit v7 debug

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Logo1 from "../assets/logo-1.png";
 import Logo2 from "../assets/logo-3.png";
 import { useEffect, useState } from "react";
