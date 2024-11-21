237a6996062aa545ac8bf7966981f451ed18440d
Victor Ezealor <75592515+uzochukwuV@users.noreply.github.com>
2024-11-20 19:33:36 -0800

    contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -7,6 +7,8 @@ const ContactPage = () => {
 
     const submitForm = ()=> {
         console.log(formState);
+        if(formState.name === '') return
+        if(formState.email === '') return
         sendMessage("Connect", formState.name, formState.email, formState.phone, formState.location, formState.info)
             .then(()=>{
                 alert("message sent")
@@ -23,44 +25,44 @@ const ContactPage = () => {
     }
 
   return (
-    <div className=" min-h-screen py-8">
-      <div className="h-full  max-w-3xl mx-auto border rounded-xl ">
-      <div className=" h-full pt-12">
-            <div autoFocus className=" relative mx-2  flex-1 bg-white rounded-t-xl shadow">
+    <div className=" min-h-screen py-8 bg-[url(https://img.freepik.com/premium-photo/african-american-young-developer-wearing-headset-while-typing-security-codes_482257-26782.jpg?ga=GA1.1.1574708294.1731893172&semt=ais_hybrid)] bg-cover">
+      <div className="h-full  max-w-2xl my-8  backdrop-blur-3xl shadow-xl mx-6 backdrop-opacity-60  mx-auto shadow-md rounded-xl ">
+      <div className=" h-full z-50 pt-12">
+            <div autoFocus className=" relative mx-2  flex-1 rounded-t-xl shadow">
                 
                 
                 </div>
-                <div className="h-full px-6 space-y-4">
+                <form className="h-full px-6 space-y-4">
                     <div>
-                    <h3 className="text-black font-bold text-3xl pb-1">TIIMAT SOLUTIONS</h3>
-                    <p className="text-[#333] font-semibold">Connect with us</p>
+                    <h3 className=" text-3xl pb-1 text-white">TIIMAT SOLUTIONS</h3>
+                    <p className="text-[#ddd]">Connect with us</p>
                     </div>
-                    <div className=" space-y-3 pb-6">
+                    <div className=" *:text-[#fdfdfd] space-y-3 pb-6">
                         <div>
                             <label htmlFor="name">Your Full Name</label>
-                            <input type="text" name="name" onChange={onChange} value={formState.name || ''}  id="name" placeholder="" className="w-full px-6 flex-1 block relative rounded-xl focus:outline-gray-300   h-14 border " />
+                            <input type="text" required name="name" onChange={onChange} value={formState.name || ''}  id="name" placeholder="" className="w-full px-6 bg-transparent flex-1 block relative rounded-xl focus:outline-gray-300   h-14 border " />
                         </div>
                         <div>
                             <label htmlFor="name">Your Email</label>
-                            <input type="email" name="email" onChange={onChange} value={formState.email || ''}   id="email" placeholder="" className="w-full px-6 flex-1 block relative rounded-xl focus:outline-gray-300   h-14 border " />
+                            <input type="email" required name="email" onChange={onChange} value={formState.email || ''}   id="email" placeholder="" className="w-full px-6 bg-transparent flex-1 block relative rounded-xl focus:outline-gray-300   h-14 border " />
                         </div>
                         <div>
                             <label htmlFor="name">Your Phone Number</label>
-                            <input type="text" name="phone" onChange={onChange} value={formState.phone || ''}   id="phone" placeholder="" className="w-full px-6 flex-1 block relative rounded-xl focus:outline-gray-300   h-14 border " />
+                            <input type="text" required name="phone" onChange={onChange} value={formState.phone || ''}   id="phone" placeholder="" className="w-full px-6 bg-transparent flex-1 block relative rounded-xl focus:outline-gray-300   h-14 border " />
                         </div>
                         <div>
                             <label htmlFor="name">Course of Interest</label>
-                            <input type="text" name="info" onChange={onChange} value={formState.info || ''}   id="info" placeholder="" className="w-full px-6 flex-1 block relative rounded-xl focus:outline-gray-300   h-14 border " />
+                            <input type="text" name="info" onChange={onChange} value={formState.info || ''}   id="info" placeholder="" className="w-full px-6 bg-transparent flex-1 block relative rounded-xl focus:outline-gray-300   h-14 border " />
                         </div>
                         <div>
                             <label htmlFor="name">Your Location</label>
-                            <input type="text" name="location" onChange={onChange} value={formState.location || ''}   id="location" placeholder="" className="w-full px-6 flex-1 block relative rounded-xl focus:outline-gray-300   h-14 border " />
+                            <input type="text" name="location" onChange={onChange} value={formState.location || ''}   id="location" placeholder="" className="w-full px-6 bg-transparent flex-1 block relative rounded-xl focus:outline-gray-300   h-14 border " />
                         </div>
                         <div>
-                            <button onClick={submitForm} className="hover:border-white block w-full h-14 hover:opacity-90 border border-white px-4 py-2 rounded-xl block bg-black text-white">Submit</button>
+                            <button onSubmit={submitForm} className="hover:border-white block w-full h-14 hover:opacity-90 b px-4 py-2 rounded-xl block bg-indigo-700 text-white">Submit</button>
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
       </div>
       </div>
