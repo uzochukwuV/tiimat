2442c261dd34eb97411a49b000e66b37e930eb7b
Victor Ezealor <75592515+uzochukwuV@users.noreply.github.com>
2024-11-29 11:07:42 +0100

    Update layout.tsx

--- src/admin/layout.tsx
+++ src/admin/layout.tsx
@@ -9,7 +9,7 @@ export function AdminLayout() {
       </div>
       <div className="sm:grid sm:grid-cols-4 w-full flex-1 ">
         <div className=" col-span-1 flex overflow-scroll py-6 px-6 md:flex md:overflow-hidden md:flex-col md:gap-4 md: ">
-         <div className=""><Link className=" min-w-40 text-center block text-black hover:underline md:text-start md:shadow md:px-6 md:py-1  " to={"/admin/course/edit/curriculum"}  >Edit curriculum</Link></div> 
+         <div className=""><Link className=" min-w-40 text-center block text-black hover:underline md:text-start md:shadow md:px-6 md:py-1  " to={"/admin/course/edit/curriculum"}  >Add curriculum</Link></div> 
          <div className=""><Link className=" min-w-40 text-center block text-black hover:underline md:text-start md:shadow md:px-6 md:py-1 " to={"/admin/course/add"} >Add Course</Link></div> 
          <div className=""><Link className=" min-w-40 text-center block text-black hover:underline md:text-start md:shadow md:px-6 md:py-1 " to={"/admin/faculty/add"} >Add Faculty </Link></div> 
          <div className=""><Link className=" min-w-40 text-center block text-black hover:underline md:text-start md:shadow md:px-6 md:py-1 " to={"/admin"}>Admin</Link></div> 
