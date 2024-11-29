3b4e26826b85058ca59fcd60fd026f9d576b2483
Victor Ezealor <75592515+uzochukwuV@users.noreply.github.com>
2024-11-29 11:30:39 +0100

    Update AddCourse.tsx

--- src/admin/AddCourse.tsx
+++ src/admin/AddCourse.tsx
@@ -94,7 +94,7 @@ function AddCourse() {
           placeholder="image Link"
           className=" px-6 flex-1  w-[80vw] block relative rounded-xl focus:outline-gray-300 ring ring-white  h-14  bg-[var(--background)]"
         />
-                <button onClick={addcoursetofaculty}  disabled={loading} className=" mt-4 bg-black hover:opacity-90 px-4 h-14 rounded-xl text-white flex gap-1 justify-center items-center" >Add <span><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" fill="#fff" width="20" height="20" viewBox="0 0 48 48">
+                <button onClick={addcoursetofaculty}  disabled={loading} className=" disabled:bg-red-400 mt-4 bg-black hover:opacity-90 px-4 h-14 rounded-xl text-white flex gap-1 justify-center items-center" >Add <span><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" fill="#fff" width="20" height="20" viewBox="0 0 48 48">
 <path d="M 5.4453125 4.0019531 A 1.50015 1.50015 0 0 0 4.109375 6.0644531 L 11.380859 24 L 4.109375 41.935547 A 1.50015 1.50015 0 0 0 6.1699219 43.841797 L 43.169922 25.341797 A 1.50015 1.50015 0 0 0 43.169922 22.658203 L 6.1699219 4.1582031 A 1.50015 1.50015 0 0 0 5.4453125 4.0019531 z M 8.3828125 8.6191406 L 39.146484 24 L 8.3828125 39.380859 L 14.011719 25.5 L 27.5 25.5 A 1.50015 1.50015 0 1 0 27.5 22.5 L 14.011719 22.5 L 8.3828125 8.6191406 z"></path>
 </svg></span></button>
               {loading && "......"}
