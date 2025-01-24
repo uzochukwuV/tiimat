1ef9ffc5c4a6bec333051f98c3b3592e8a318a73
uzochukwuv <vic.ezealor@gmail.com>
2025-01-24 11:36:10 +0100

    new front dclen

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,7 @@
 import { Outlet } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
-
 import { useEffect, useState } from "react";
-import { getAllCourse, getFaculties } from "./services/read";
+import { getAllCourse } from "./services/read";
 import Navigation from "./screen/ui/Header";
 import { Footer } from "./screen/ui/Footer";
 
@@ -11,15 +9,10 @@ import { Footer } from "./screen/ui/Footer";
 
 export const Layout = () => {
   const [courses, setCourses] = useState([]) as any;
-  const [faculty, setFaculty] = useState() as any;
   useEffect(() => {
     getAllCourse()
       .then((data) => {
         setCourses(data)
-      })
-    getFaculties()
-      .then((data) => {
-        setFaculty(data)
       })
   }, [])
 
@@ -28,7 +21,6 @@ export const Layout = () => {
     <>
 
       <section id="app" className=' overflow-hidden absolute top-0 left-0 right-0 bg-[var(--background)] min-h-screen'>
-        {/* <Navbar faculty={faculty} /> */}
         <Navigation />
         <Outlet />
         <Footer courses={courses} />
