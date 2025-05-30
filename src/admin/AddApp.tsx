70257264528554ce083bbeb7a9a13a994d7a524f
uzochukwuv <vic.ezealor@gmail.com>
2025-05-30 16:49:17 +0100

    added app data

---
+++ src/admin/AddApp.tsx
@@ -0,0 +1,30 @@
+
+import { DataTable } from "./data/data-table";
+import { appDataColumns } from "./data/columns";
+import { useLoaderData } from "react-router-dom";
+import {  updateAppData } from "@/services/actions";
+import { toast } from "sonner";
+import { AppDataType } from "@/services/types";
+
+
+
+
+export function AppAdmin() {
+  const loader = useLoaderData() as AppDataType[];
+  // const navigate = useNavigate()
+  const handleDelete=async ( payload:any)=>{
+    console.log(payload)
+    try {
+    //   await deleteAdminCurriculum({payload:payload})
+      toast("Item deleted Successfully")
+      // navigate(0)
+    } catch (error) {
+      console.log(error)
+      toast("Item delete error try again")
+    }
+   
+  }
+  return (<>
+      <DataTable columns={appDataColumns} data={loader} create={updateAppData} deleteItems={handleDelete} />
+  </>)
+}
