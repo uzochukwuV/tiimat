1ccb936edd83ca82bd7db34b3690ffc71a50d2f3
uzochukwuv <vic.ezealor@gmail.com>
2025-06-20 16:38:26 +0100

    commit message

---
+++ src/admin/Message.tsx
@@ -0,0 +1,48 @@
+import {  useEffect, useState } from "react"
+import {  getAllCourse } from "../services/read";
+import { useLoaderData, useParams } from "react-router-dom";
+import { Message, MessageColumns, Trimester, trimesterColumns } from "./data/columns";
+import { DataTable } from "./data/data-table";
+import { createAdminTimester, createMessage, deleteAdminTrimester } from "@/services/actions";
+import { toast } from "sonner";
+
+
+
+export function MessageAdmin() {
+  const loader = useLoaderData() as Message[];
+  const params = useParams()
+  // const navigate = useNavigate()
+
+  const [option, setOption] = useState({})
+
+  const handleDelete=async ( payload:any)=>{
+    console.log(payload)
+    try {
+      await deleteAdminTrimester({payload:payload})
+      toast("Item deleted Successfully")
+      // navigate(0)
+    } catch (error) {
+      console.log(error)
+      toast("Item delete error try again")
+    }
+   
+  }
+
+  
+
+  const createTrimtester = async ({payload}:any)=>{
+    try {
+      await createMessage({payload:{...payload, courseId:params.id}})
+      toast("Item created Successfully")
+      // navigate(0)
+    }
+    catch (error) {
+      console.log(error)
+      toast("Item create error try again")
+    }
+  }
+  
+  return (<>
+      <DataTable columns={MessageColumns} data={loader} deleteItems={handleDelete} create={createTrimtester} optionInput={option} id="courseId"  />
+  </>)
+}
--- src/admin/createCertificate.tsx
+++ src/admin/createCertificate.tsx
@@ -47,8 +47,6 @@ export function CreateCertificate() {
           
           setData(data)
           
-        }).catch((error) => {
-          
         })
       }, [])
     
