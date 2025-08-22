067d9b2c610bdf1582931d655c4df3b7d39fb183
Victor Ezealor <75592515+uzochukwuV@users.noreply.github.com>
2025-08-22 11:42:07 +0100

    new update

--- src/admin/createCertificate.tsx
+++ src/admin/createCertificate.tsx
@@ -21,7 +21,7 @@ export function CreateCertificate() {
         const name = (document.getElementById('name') as HTMLInputElement).value
         const description = (document.getElementById('description') as HTMLInputElement).value
         const image = (document.getElementById('image') as HTMLInputElement).files![0]
-       
+       console.log(image)
         try {
              let res = await uploadFile("tiimatbucket", "certificates", image);
              const newFile = URL.createObjectURL(image);
